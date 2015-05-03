c2390f76d690851517c8d9ec2ec1eb6a8033c5ba
dan <dan.bykov@gmail.com>
2015-05-03 19:12:02 +0300

    update generators

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,17 @@ Bundler.require(*Rails.groups)
 
 module TestApp
   class Application < Rails::Application
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.serializer false
+      g.view_specs false
+      g.helper_specs false
+      g.controller_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
